b37af2e5a19752df0ba94a82b0eed52f2c50ab8f
Gabriel Mendes <gabriel.mendes@ciss.com.br>
2022-04-06 20:15:09 -0300

    Service layer and Component registration

--- src/main/java/com/gabrielmendes/course/resources/UserResource.java
+++ src/main/java/com/gabrielmendes/course/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.gabrielmendes.course.resources;
 
 import com.gabrielmendes.course.entities.User;
+import com.gabrielmendes.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L,"Maria", "maria@gmailcom", "9999", "1234");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> userList = userService.findAll();
+
+        return ResponseEntity.ok().body(userList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+
+        return ResponseEntity.ok().body(user);
     }
 
 }
