7347b1d5f38c615c01f26792640b58b88758ab15
Gabriel Mendes <gabriel.mendes@ciss.com.br>
2022-04-07 13:08:03 -0300

    Product

--- src/main/java/com/gabrielmendes/course/entities/Category.java
+++ src/main/java/com/gabrielmendes/course/entities/Category.java
@@ -5,7 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -15,6 +18,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category(){
 
@@ -39,6 +45,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
---
+++ src/main/java/com/gabrielmendes/course/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.gabrielmendes.course.repositories;
+
+import com.gabrielmendes.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+}
