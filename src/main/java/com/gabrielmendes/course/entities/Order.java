a746de6eeebdc61f656dd727ab57c147377f5cac
Gabriel Mendes <gabriel.mendes@ciss.com.br>
2022-04-09 14:22:50 -0300

    Payment, one-to-one

--- src/main/java/com/gabrielmendes/course/entities/Order.java
+++ src/main/java/com/gabrielmendes/course/entities/Order.java
@@ -3,6 +3,7 @@ package com.gabrielmendes.course.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gabrielmendes.course.entities.enums.OrderStatus;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.Instant;
@@ -36,6 +38,9 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -84,6 +89,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
