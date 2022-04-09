51247c54320b304a11d056b7cc1e628456707457
Gabriel Mendes <gabriel.mendes@ciss.com.br>
2022-04-09 16:33:11 -0300

    Exception handling - update

--- src/main/java/com/gabrielmendes/course/services/UserService.java
+++ src/main/java/com/gabrielmendes/course/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,10 +43,14 @@ public class UserService {
     }
 
     public User update(Long id, User user){
-        User userEntity = userRepository.getById(id);
-        updateData(userEntity, user);
+        try {
+            User userEntity = userRepository.getById(id);
+            updateData(userEntity, user);
 
-        return userRepository.save(userEntity);
+            return userRepository.save(userEntity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User userEntity, User user) {
